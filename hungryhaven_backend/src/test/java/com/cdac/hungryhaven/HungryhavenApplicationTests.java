02f0145ff3f4b083529486a760a0fcaca3c81cdc
Pawan Didwaniya <pawandidwaniya7@gamil.com>
2023-08-14 20:26:46 +0530

    initial commit

---
+++ hungryhaven_backend/src/test/java/com/cdac/hungryhaven/HungryhavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.hungryhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryhavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
