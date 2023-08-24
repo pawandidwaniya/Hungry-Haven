f1205b0020e06c722f1c424be01bf65cb5256642
Nishant <nishu10092000@gmail.com>
2023-08-24 13:52:27 +0530

    Made exceptions class

---
+++ hungryhaven_backend/src/main/java/com/cdac/hungryhaven/exceptions/ItemNotFoundInRestaurantMenuException.java
@@ -0,0 +1,10 @@
+package com.cdac.hungryhaven.exceptions;
+
+public class ItemNotFoundInRestaurantMenuException extends RuntimeException {
+    
+    public ItemNotFoundInRestaurantMenuException(String message) {
+        super(message);
+    }
+    
+    // You can add additional constructors or methods if needed
+}
---
+++ hungryhaven_backend/src/main/java/com/cdac/hungryhaven/exceptions/ItemNotFromSameRestaurantException.java
@@ -0,0 +1,10 @@
+package com.cdac.hungryhaven.exceptions;
+
+public class ItemNotFromSameRestaurantException extends RuntimeException {
+    
+    public ItemNotFromSameRestaurantException(String message) {
+        super(message);
+    }
+    
+    // You can add additional constructors or methods if needed
+}
