0271bbbc5706e1f2742a3245de2b4207cf87d474
AbhinavKrr <77889408+AbhinavKrr@users.noreply.github.com>
2023-09-08 20:10:04 +0530

    Delete hungryhaven_backend/src/main/java/com/cdac/hungryhaven/repositories directory

--- hungryhaven_backend/src/main/java/com/cdac/hungryhaven/repositories/RestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cdac.hungryhaven.repositories;
-
-import com.cdac.hungryhaven.models.RestaurantEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantRepository extends JpaRepository<RestaurantEntity, Long> {
-}
